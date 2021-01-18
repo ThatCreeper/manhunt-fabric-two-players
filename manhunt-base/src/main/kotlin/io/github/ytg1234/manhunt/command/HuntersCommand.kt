ac9d64069aaa071c2bfeba4ea859b003e25357be
YTG123 <54103478+YTG1234@users.noreply.github.com>
2021-01-18 21:16:06 +0200

    Hunters command done, hopefully

--- manhunt-base/src/main/kotlin/io/github/ytg1234/manhunt/command/HuntersCommand.kt
+++ manhunt-base/src/main/kotlin/io/github/ytg1234/manhunt/command/HuntersCommand.kt
@@ -3,10 +3,10 @@ package io.github.ytg1234.manhunt.command
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.context.CommandContext
 import io.github.ytg1234.manhunt.base.CONFIG
-import io.github.ytg1234.manhunt.base.fromCmdContext
 import io.github.ytg1234.manhunt.base.UserVars.hunters
-import io.github.ytg1234.manhunt.base.playerHasMod
 import io.github.ytg1234.manhunt.base.UserVars.speedrunners
+import io.github.ytg1234.manhunt.base.fromCmdContext
+import io.github.ytg1234.manhunt.base.ifHasMod
 import io.github.ytg1234.manhunt.util.PermedCommand
 import io.github.ytg1234.manhunt.util.plus
 import io.github.ytg1234.manhunt.util.reset
@@ -18,7 +18,6 @@ import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.text.LiteralText
 import net.minecraft.text.TranslatableText
-import java.util.ArrayList
 
 /**
  * Used to manage the [hunter list][hunters].
@@ -45,13 +44,15 @@ object HuntersCommand : PermedCommand("hunters", "manhunt.command.hunters", 2) {
      * Clears the hunter list.
      */
     private fun executeClear(context: CommandContext<ServerCommandSource>): Int {
-        val playerHasMod: Boolean = playerHasMod(context)
         hunters.clear()
-        if (playerHasMod) {
-            context.source.sendFeedback(TranslatableText("text.manhunt.command.hunters.clear"), true)
-        } else {
-            context.source.sendFeedback(LiteralText("Cleared hunter list!"), false)
-        }
+        context.source.sendFeedback(
+            ifHasMod(
+                context,
+                TranslatableText("text.manhunt.command.hunters.clear"),
+                LiteralText("Cleared hunter list!")
+            ),
+            true
+        )
         return Command.SINGLE_SUCCESS
     }
 
@@ -60,43 +61,34 @@ object HuntersCommand : PermedCommand("hunters", "manhunt.command.hunters", 2) {
      */
     private fun executeAdd(context: CommandContext<ServerCommandSource>): Int {
         val target = EntityArgumentType.getPlayer(context, "target")
-        val playerHasMod: Boolean = playerHasMod(context)
 
         // Target is speedrunner
-        if (target.uuid == speedrunners.orElse(null)) { // hacky optional work here
-            // We check if the source is a player and the player has the mod
-            if (playerHasMod) {
-                // We send error message using TranslatableText
-                context.source.sendError(
+        if (speedrunners.contains(target.uuid)) {
+            context.source.sendError(
+                ifHasMod(
+                    context,
                     TranslatableText(
                         "text.manhunt.command.hunters.error.speedrunner",
                         target.displayName
-                    )
+                    ),
+                    LiteralText("Cannot add ") + target.displayName + LiteralText(" as a hunter because they are a speedrunner!")
                 )
-
-                // Source is not player or doesn't have mod, sending error message using LiteralText
-            } else {
-                context.source
-                    .sendError(
-                        LiteralText("Cannot add ").append(target.displayName)
-                            .append(LiteralText(" as a hunter because they are the speedrunner!"))
-                    )
-            }
+            )
             return Command.SINGLE_SUCCESS
         }
 
         // Check if target is already a hunter
         if (hunters.contains(target.uuid)) {
-            if (playerHasMod) {
-                context.source.sendError(
+            context.source.sendError(
+                ifHasMod(
+                    context,
                     TranslatableText(
                         "text.manhunt.command.hunters.error.hunter",
                         target.displayName
-                    )
+                    ),
+                    reset("Cannot add ") + target.displayName + reset(" as a hunter because they are already a hunter!")
                 )
-            } else {
-                context.source.sendError(reset("Cannot add ") + target.displayName + reset(" as a hunter because they are already a hunter!"))
-            }
+            )
             return Command.SINGLE_SUCCESS
         }
         if (CONFIG!!.giveCompassWhenSettingHunters) fromCmdContext(context, target.uuid)!!.equip(
@@ -104,14 +96,15 @@ object HuntersCommand : PermedCommand("hunters", "manhunt.command.hunters", 2) {
             ItemStack(Items.COMPASS, 1)
         )
         hunters.add(target.uuid)
-        if (playerHasMod) {
-            context.source.sendFeedback(TranslatableText("text.manhunt.command.hunters.add", target.displayName), true)
-        } else {
-            context.source
-                .sendFeedback(
-                    reset("Added ") + target.displayName + reset(" to the hunters list!"), true
-                )
-        }
+
+        context.source.sendFeedback(
+            ifHasMod(
+                context,
+                TranslatableText("text.manhunt.command.hunters.add", target.displayName),
+                reset("Added ") + target.displayName + reset(" to the hunters list!")
+            ),
+            true
+        )
         return Command.SINGLE_SUCCESS
     }
 
@@ -119,24 +112,22 @@ object HuntersCommand : PermedCommand("hunters", "manhunt.command.hunters", 2) {
      * Sends the hunter list.
      */
     private fun executeGet(context: CommandContext<ServerCommandSource>): Int {
-        val playerHasMod: Boolean = playerHasMod(context)
         if (hunters.isEmpty()) return 1
-        val hunterNames: MutableList<String> = ArrayList()
-        hunters.forEach { element ->
-            hunterNames.add(
-                fromCmdContext(context, element)!!.displayName.asString()
-            )
+        val hunterNames: Collection<String> = hunters.map {
+            fromCmdContext(context, it)!!.displayName.asString()
         }
-        if (playerHasMod) {
-            context.source.sendFeedback(
+
+        context.source.sendFeedback(
+            ifHasMod(
+                context,
                 TranslatableText(
                     "text.manhunt.command.hunters.get",
                     java.lang.String.join(", ", hunterNames)
-                ), false
-            )
-        } else {
-            context.source.sendFeedback(LiteralText("Hunters are: " + java.lang.String.join(", ", hunterNames)), false)
-        }
+                ),
+                LiteralText("Hunters are: " + java.lang.String.join(", ", hunterNames))
+            ),
+            true
+        )
         return Command.SINGLE_SUCCESS
     }
 }
